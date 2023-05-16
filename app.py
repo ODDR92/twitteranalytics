3c3231a04fa4ff5c80045525d68a6a49dd160d00
olivierdebeufderijcker <131962902+olivierdebeufderijcker@users.noreply.github.com>
2023-05-16 17:50:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 # app.py
 from flask import Flask, redirect, request, render_template
 from twitter_api import get_auth, get_redirect_url, get_access_token
+import requests
+import csv
+import os
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
@@ -26,4 +30,33 @@ def callback():
         # Save these tokens for later use
         return 'Successfully authorized!'
     else:
-        return
+        return 'Error! Failed to get access token.'
+
+@app.route('/fetch_tweets')
+def fetch_tweets():
+    # Get the user's tweets from the past 30 days
+    user_id = 'YOUR_USER_ID'  # Replace with the ID of the authenticated user
+    bearer_token = os.getenv('BEARER_TOKEN')
+    url = f"https://api.twitter.com/2/users/{user_id}/tweets"
+    params = {
+        'expansions': 'non_public_metrics',
+        'start_time': (datetime.now() - timedelta(days=30)).isoformat()+'Z',
+    }
+    headers = {
+        'Authorization': f'Bearer {bearer_token}',
+    }
+
+    response = requests.get(url, params=params, headers=headers)
+    tweets = response.json()['data']
+
+    # Write the tweets to a CSV file
+    with open('tweets.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Tweet ID', 'Text', 'Impressions'])
+        for tweet in tweets:
+            writer.writerow([tweet['id'], tweet['text'], tweet['non_public_metrics']['impression_count']])
+
+    return 'Successfully fetched tweets!'
+
+if __name__ == '__main__':
+    app.run()
